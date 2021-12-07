8c2b7129db61ebc569d0d631b61b250c3d0a2d38
chetan6780 <crp20802@gmail.com>
2021-12-07 21:01:11 +0530

    started with typer

--- task.py
+++ task.py
@@ -1,18 +1,10 @@
-#!/usr/bin/python3
+import typer    
 
-import click
+app = typer.Typer()
 
-@click.group()
-def cli():
-    pass
-
-@cli.command(name="add")
-def add():
-    pass
-
-@cli.command(name="help")
+@app.command()
 def help():
-    click.echo("""Usage :-
+    print("""Usage :-
 $ ./task add 2 hello world    # Add a new item with priority 2 and text "hello world" to the list
 $ ./task ls                   # Show incomplete priority list items sorted by priority in ascending order
 $ ./task del INDEX            # Delete the incomplete item with the given index
@@ -21,10 +13,16 @@ $ ./task help                 # Show usage
 $ ./task report               # Statistics
 """)
     
-@cli.command(name="")
-def ls(): 
+@app.command()
+def add(priority: int, task: str):
+    """Add a new item with priority <priority> and text <text> to the list"""
+    print(f"Added item with priority {priority} and text {task}")
 
-
-
+    
+@app.command()
+def done(itemNum:int):
+    print("Marked item as done.")
+    
 if __name__ == '__main__':
-    cli()
+    app()
+    
