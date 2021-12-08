91a745858c37e4d7952ab322c3efa97d97540f8f
chetan6780 <crp20802@gmail.com>
2021-12-08 21:16:48 +0530

    Documented

--- task.py
+++ task.py
@@ -22,17 +22,20 @@ $ ./task report               # Statistics
 def add(priority: Optional[int] = typer.Argument(-1), text: Optional[str] = typer.Argument(None)):
     """Add a new item with priority <priority> and text <text> to the list"""
 
+    # If priority is not specified, return Error
     if priority == -1:
         typer.echo("Error: Missing tasks string. Nothing added!")
         exit()
 
+    # If text is not specified, return Error
     if text is None:
         typer.echo("Error: Missing tasks string. Nothing added!")
         exit()
 
     priority_index = 0
-    tasks = []
+    tasks = []  # Tasks List
 
+    # If file is already present
     try:
         # find less than or equal to priority index
         with open("task.txt", "r") as file:
@@ -54,9 +57,11 @@ def add(priority: Optional[int] = typer.Argument(-1), text: Optional[str] = type
                     if i != priority_index:
                         file.write(tasks[i])
                     else:
+                        # New task at sorted correct position
                         file.write(f"{priority} {text}\n")
                         file.write(tasks[i])
 
+    # If file not present, Create a new one
     except FileNotFoundError:
         with open("task.txt", "a") as file:
             file.write(f"{priority} {text}\n")
@@ -68,19 +73,26 @@ def add(priority: Optional[int] = typer.Argument(-1), text: Optional[str] = type
 def Del(index: Optional[int] = typer.Argument(-1)):
     """Delete the incomplete item with the given index"""
 
+    # If index is not specified, return Error
     if index == -1:
         typer.echo("Error: Missing NUMBER for deleting tasks.")
         exit()
+
+    # If index is 0, Return Error with not exist message
     if index == 0:
         typer.echo(f"Error: task with index #0 does not exist. Nothing deleted.")
         exit()
 
-    delete_task = ""
+    delete_task = ""    # Task to be deleted
+
+    # If index is Valid
     try:
+        # Get the task to be deleted
         with open("task.txt", "r") as file:
             tasks = file.readlines()
             delete_task = tasks[index - 1]
 
+        # Write tasks in the file, except the task that to be deleted
         with open("task.txt", "w") as file:
             for task in tasks:
                 if task != delete_task:
@@ -88,6 +100,7 @@ def Del(index: Optional[int] = typer.Argument(-1)):
 
         typer.echo(f"Deleted task #{index}")
 
+    # If index is invalid
     except IndexError:
         typer.echo(
             f"Error: task with index #{index} does not exist. Nothing deleted.")
@@ -97,7 +110,9 @@ def Del(index: Optional[int] = typer.Argument(-1)):
 def ls():
     """Show incomplete priority list items sorted by priority in ascending order"""
 
+    # If file is present
     try:
+        # Print pending tasks, if present
         with open("task.txt", "r") as file:
             lines = file.readlines()
             lines_count = len(lines)
@@ -107,6 +122,8 @@ def ls():
                 for i in range(lines_count):
                     task = lines[i].split(" ", 1)
                     typer.echo(f"{i+1}. {task[1][:-1]} [{task[0]}]")
+
+    # If file is present
     except FileNotFoundError:
         typer.echo("There are no pending tasks!")
 
@@ -115,6 +132,7 @@ def ls():
 def report():
     """Statistics"""
 
+    # If file present, print pending tasks
     try:
         with open("task.txt", "r") as file:
             lines = file.readlines()
@@ -123,9 +141,12 @@ def report():
             for i in range(lines_count):
                 task = lines[i].split(" ", 1)
                 typer.echo(f"{i+1}. {task[1][:-1]} [{task[0]}]")
+
+    # If file not present, print pending task count 0
     except FileNotFoundError:
         typer.echo(f"Pending : {0}")
 
+    # If file is present, print completed tasks
     try:
         with open("completed.txt", "r") as file:
             lines = file.readlines()
@@ -133,6 +154,8 @@ def report():
             typer.echo(f"\nCompleted : {len(lines)}")
             for i in range(lines_count):
                 print(f"{i+1}. {lines[i]}", end="")
+
+    # If file not present, print completed task count 0
     except FileNotFoundError:
         typer.echo(f"\nCompleted : {0}")
 
@@ -141,29 +164,38 @@ def report():
 def done(index: Optional[int] = typer.Argument(-1)):
     """Mark the incomplete item with the given index as complete"""
 
+    # If index is not specified, print Error
     if index == -1:
         typer.echo("Error: Missing NUMBER for marking tasks as done.")
         exit()
+
+    # If index is 0, Print Error
     if index == 0:
         typer.echo("Error: no incomplete item with index #0 exists.")
         exit()
 
-    done_task = ""
+    done_task = ""  # Done task
+
+    # If index is Valid
     try:
+        # Get Done Task
         with open("task.txt", "r") as file:
             tasks = file.readlines()
             done_task = tasks[index - 1]
 
+        # Write all task in task.txt file, except done task
         with open("task.txt", "w") as file:
             for task in tasks:
                 if task != done_task:
                     file.write(task)
 
+        # Write done task in completed.txt file
         with open("completed.txt", "a") as file:
             file.write(done_task.split(" ", 1)[1])
 
         typer.echo("Marked item as done.")
 
+    # If index is Invalid
     except IndexError:
         typer.echo(f"Error: no incomplete item with index {index} exists.")
 
